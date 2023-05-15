09d35a70cfb8b549513958522b60988d1f768c2f
SilkRoad007 <131393457+SilkRoad007@users.noreply.github.com>
2023-05-15 16:30:42 +0200

    Add files via upload

---
+++ 1. Progress Bar in % - Responsive/script.js
@@ -0,0 +1,9 @@
+$(function () {
+    // Progress Bar Animate
+    window.sr = ScrollReveal();
+    sr.reveal(".progress-bar", {
+        origin: "left",
+        duration: 2000,
+        distance: "100%"
+    });
+});
